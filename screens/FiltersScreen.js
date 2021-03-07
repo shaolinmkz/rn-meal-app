9230f8da7041fa221dee8851ef183d80194cf416
shaolinmkz007 <emeka@softcom.ng>
2021-03-07 18:47:09 +0100

    filters page logic

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,30 +1,112 @@
-import React from 'react';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { StyleSheet, Text, View } from 'react-native';
-import CustomHeaderButton from '../components/CustomHeaderButton';
+import React, { useState, useEffect, useCallback } from "react";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { StyleSheet, Text, View, Switch, Platform } from "react-native";
+import CustomHeaderButton from "../components/CustomHeaderButton";
+import colors from "../constants/colors";
 
-const FiltersScreen = () => {
+const Filters = ({ callback, label }) => {
+  const [filterToggle, setFilterToggle] = useState(false);
+
+  const handleFilterToggle = (val) => {
+    setFilterToggle(val);
+    callback?.(label, val);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Filters Screen!</Text>
+    <View style={styles.filterContainer}>
+      <Text>{label}</Text>
+      <Switch
+        trackColor={{ true: colors.primary, false: colors.secondary }}
+        thumbColor={Platform.OS === "android" ? colors.primary : colors.white}
+        value={filterToggle}
+        onValueChange={handleFilterToggle}
+      />
     </View>
   );
-}
+};
+
+const FiltersScreen = ({ navigation }) => {
+  const [state, setState] = useState({
+    "Gluten-free": false,
+    "Lactose-free": false,
+    Vegan: false,
+    Vegetarian: false,
+  });
+
+  const handleFilterChange = (label, value) => {
+    setState((prevState) => ({ ...prevState, [label]: value }));
+  };
+
+  const save = useCallback(() => {
+    const updatedState = {
+      glutenFree: state["Gluten-free"],
+      lactoseFree: state["Lactose-free"],
+      vegan: state.Vegan,
+      vegetarian: state.Vegetarian,
+    };
+
+    console.log(updatedState);
+  }, [
+    state["Gluten-free"],
+    state["Lactose-free"],
+    state.Vegan,
+    state.Vegetarian,
+  ]);
+
+  useEffect(() => {
+    navigation.setParams({ save });
+  }, [save]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <Filters label="Gluten-free" callback={handleFilterChange} />
+      <Filters label="Lactose-free" callback={handleFilterChange} />
+      <Filters label="Vegan" callback={handleFilterChange} />
+      <Filters label="Vegetarian" callback={handleFilterChange} />
+    </View>
+  );
+};
 
 FiltersScreen.navigationOptions = (navData) => ({
   headerTitle: "Filters",
   headerLeft: () => (
     <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-      <Item title="Menu" iconName="ios-menu" onPress={() => navData.navigation.toggleDrawer()} />
+      <Item
+        title="Menu"
+        iconName="ios-menu"
+        onPress={() => navData.navigation.toggleDrawer()}
+      />
     </HeaderButtons>
-  )
+  ),
+  headerRight: ({ navigation }) => (
+    <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item
+        title="Save"
+        iconName="ios-save"
+        onPress={() => navigation?.getParam?.("save")?.()}
+      />
+    </HeaderButtons>
+  ),
 });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 20,
+    margin: 20,
+    textAlign: "center",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    marginVertical: 10,
   },
 });
 
